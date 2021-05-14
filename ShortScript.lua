ec82e4308c32ff4a184ceeec68e6b49623a7ec53
Ibrahim Musayev <84103775+Codehunter-py@users.noreply.github.com>
2021-05-14 15:52:02 +0200

    Update ShortScript.lua

--- ShortScript.lua
+++ ShortScript.lua
@@ -1,5 +1,14 @@
 local naloMesh= script.parent
+local numberOfPlayers = 3
 
 naloMesh:PlayAnimation("unarmed_wave")
 
+-- Conditional
 
+if numberOfPlayers > 3 then
+print("We have more than 3 players!")
+elseif numberOfPlayers == 3 then
+print("Almost enough players")
+else
+print("Not enough players")
+end
